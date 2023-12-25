0e92ec8c53390f3ca6080b060072a67bacd1a04b
vukang <75476802+vukang@users.noreply.github.com>
2023-12-26 00:08:15 +0100

    initial commit gh-pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import SplitPane from 'react-split-pane';
+import { marked } from 'marked';
+import { useState } from 'react';
+
+marked.setOptions({
+  gfm: true,
+  breaks: true,
+});
+
+// import marked parser
+// import SplitPane https://www.npmjs.com/package/react-split-pane
 
 function App() {
+  const [md, setMd] = useState(`# Marked in Node.js
+
+## Live markdown previewer
+  
+> Blockquote
+  
+* list item
+
+\`inlinecode with backticks\`
+
+    codeblock indented by 4 spaces or tab
+  
+My favorite search engine is [Duck Duck Go](https://duckduckgo.com).
+
+![Tux, the Linux mascot](/assets/images/tux.png)
+
+---
+  
+Rendered by **marked**.`);
+
+  const handleChange = (e) => {
+    setMd(e.currentTarget.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <SplitPane split='vertical' minSize={100}>
+      <div style={{ height: '100vh' }}>
+        <textarea
+          id='editor'
+          style={{ minHeight: '100vh', width: '99%' }}
+          onChange={handleChange}
+          value={md}
+        ></textarea>
+      </div>
+      <div
+        id='preview'
+        style={{ minHeight: '100vh' }}
+        dangerouslySetInnerHTML={{ __html: marked.parse(md) }}
+      ></div>
+    </SplitPane>
   );
 }
 
